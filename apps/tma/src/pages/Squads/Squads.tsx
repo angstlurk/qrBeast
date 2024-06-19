30547a6beb1352fe11f7cc9fb4769e9cb49029df
Pipkop <angst.name@gmail.com>
2024-06-19 15:32:22 +0700

    Fix share invite friends

--- apps/tma/src/pages/Squads/Squads.tsx
+++ apps/tma/src/pages/Squads/Squads.tsx
@@ -1,9 +1,9 @@
-import { useInitData, useUtils } from "@tma.js/sdk-react";
+import { useInitData } from "@tma.js/sdk-react";
+import { Button } from "konsta/react";
 import QRCode from "react-qr-code";
 
 const Squads = () => {
   const data = useInitData();
-  const utils = useUtils();
 
   if (data === undefined || data.user === undefined) {
     return <div>Please use in telegram</div>;
@@ -12,21 +12,19 @@ const Squads = () => {
   const referalUrl = `https://t.me/qrBeastBot/start?startapp=kentId${data.user.id}`;
 
   return (
-    <div className="w-full h-full flex flex-col align-center justify-center items-center gap-4">
-      <span className="font-semibold">
-        Click on the qr code to share the game with friends and get a reward
+    <div className="w-full h-full flex flex-col align-center justify-center items-center gap-4 p-10">
+      <span className="text-white text-lg text-center">
+        Invite your friends with this this qr link or share app by button
       </span>
-      <QRCode
-        bgColor="#374151"
-        onClick={() => {
-          utils.shareURL(referalUrl, "Go to the app to get your reward.");
-        }}
-        value={referalUrl}
-      />
-      <span className="font-semibold">
-        Share this link to invite your friends and earn rewards together!
-      </span>
-      <span>{referalUrl}</span>
+      <a>
+        <QRCode bgColor="#374151" value={referalUrl} />
+      </a>
+
+      <a
+        href={`https://t.me/share/url?url=${referalUrl}&text=Play with me and get jettons`}
+      >
+        <Button className="px-18">Invite friends</Button>
+      </a>
     </div>
   );
 };
