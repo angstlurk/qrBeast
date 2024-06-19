6c1812fc9c98fa27733e5c7c5c1c3717d8f7a43d
Pipkop <angst.name@gmail.com>
2024-06-19 15:34:07 +0700

    Fix other app quest

--- apps/tma/src/pages/Quests/LongQuest.tsx
+++ apps/tma/src/pages/Quests/LongQuest.tsx
@@ -1,21 +1,30 @@
-import { useMiniApp } from "@tma.js/sdk-react";
+import { useMiniApp, useUtils } from "@tma.js/sdk-react";
+import { Button } from "konsta/react";
 
 export const SecondQuest = () => {
+  const utils = useUtils();
   const miniApp = useMiniApp();
 
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <a
-        className="text-white w-1/2 text-center"
-        onClick={() => {
-          setTimeout(() => {
-            miniApp.close();
-          }, 1000);
-        }}
-        href="https://t.me/qrMonster_bot/start"
-      >
+    <div className="w-full h-full flex flex-col items-center justify-center gap-3">
+      <span className="text-white w-1/2 text-center">
         Go to another telegram mini application to get a reward
-      </a>
+      </span>
+
+      <div>
+        <Button
+          onClick={() => {
+            utils.openTelegramLink("https://t.me/qrMonster_bot/start");
+
+            setTimeout(() => {
+              miniApp.close();
+            }, 1000);
+          }}
+          className="px-20"
+        >
+          Let's go
+        </Button>
+      </div>
     </div>
   );
 };
